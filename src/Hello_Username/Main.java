3296f9538b790c10cf10dd75d5fdb48f10a425d6
Andrei Pakudin <andrey.pakudin@yandex.ru>
2025-07-17 10:24:20 +0300

    Commit

--- src/Hello_Username/Main.java
+++ src/Hello_Username/Main.java
@@ -1,4 +1,13 @@
 package Hello_Username;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name, please: ");
+        String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
+    }
 }
